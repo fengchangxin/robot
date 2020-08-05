8a0782138caa0a712e03529e68efd31d6ce124e2
fengchangxin <1534891720@qq.com>
2020-08-05 23:06:07 +0800

    初始化

--- src/components/HelloFromVux.vue
+++ src/components/HelloFromVux.vue
@@ -1,22 +1,18 @@
 <template>
   <div>
     <div class="vux-demo">
-      <img class="logo" src="../assets/vux_logo.png">
-      <h1> </h1>
+
     </div>
-    <group title="cell demo">
-      <cell title="VUX" value="cool" is-link></cell>
-    </group>
+    <icon type="warn"></icon>
   </div>
 </template>
 
 <script>
-import { Group, Cell } from 'vux'
+import { Icon } from 'vux'
 
 export default {
   components: {
-    Group,
-    Cell
+    Icon
   },
   data () {
     return {
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloFromVux from '@/components/HelloFromVux'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'HelloFromVux',
+      component: HelloFromVux
     }
   ]
 })
